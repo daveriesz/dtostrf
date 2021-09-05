99241c9e75d9fbf7a5e4f2358d8819ed43b94906
daveriesz <dave@riesz.net>
2021-09-05 12:29:34 -0700

    little of consequence

--- dtostrf.c
+++ dtostrf.c
@@ -3,7 +3,18 @@
 
 char *dtostrf(double v, signed char w, unsigned char p, char *s)
 {
+  char *cp = s;
+  int whlen;, ii;
+  double vt;
 
+  // whlen will be the length of the whole part of the number
+  for(whlen=0, vt=v ; vt>=0.0 ; whlen++) { vt /= 10.0; }
+  if(whlen == 0) { whlen = 1; } // gotta have something there
+
+  vt = v - ((double)((long long)v))
+  for(ii=0 ; ii<p ; ii++)
+
+  
   s[0]='\0';
   return s;
 }
