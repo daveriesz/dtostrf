32e80163d4fae356587efd23fc32c54a96f460c4
daveriesz <dave@riesz.net>
2021-09-05 18:46:37 -0700

    transfer

--- dtostrf.c
+++ dtostrf.c
@@ -1,18 +1,40 @@
 
 #include <stdio.h>
 
+double xround(double v, int p)
+{
+  int ii;
+  double v2, v3;
+
+  v2 = v;
+  if(p<0) { for(ii=0 ; ii>p ; ii--) { v2 *= 10.0; } }
+  if(p>0) { for(ii=0 ; ii<p ; ii++) { v2 /= 10.0; } }
+
+//  v3 = ((double)(((long long)(v2 - ((double)((long long)v2)) * 10.0))));
+  v3 = ((double)((long long)((v2 - ((double)((long long)v2))) * 10.0)));
+  v2 = ((double)((long long)v2));
+  if(v3 >= 5.0) { v2 += 1.0; }
+  printf("v2 = %.5f\n", v2);
+  printf("v3 = %.5f\n", v3);
+  
+  if(p<0) { for(ii=0 ; ii>p ; ii--) { v2 /= 10.0; } }
+  if(p>0) { for(ii=0 ; ii<p ; ii++) { v2 *= 10.0; } }
+
+  return v2;
+}
+
 char *dtostrf(double v, signed char w, unsigned char p, char *s)
 {
   char *cp = s;
-  int whlen;, ii;
+  int whlen, ii;
   double vt;
 
   // whlen will be the length of the whole part of the number
   for(whlen=0, vt=v ; vt>=0.0 ; whlen++) { vt /= 10.0; }
   if(whlen == 0) { whlen = 1; } // gotta have something there
 
-  vt = v - ((double)((long long)v))
-  for(ii=0 ; ii<p ; ii++)
+  vt = v - ((double)((long long)v));  
+  for(ii=0 ; ii<p ; ii++);
 
   
   s[0]='\0';
--- test.c
+++ test.c
@@ -3,8 +3,37 @@
 #include <string.h>
 
 char *dtostrf(double __val, signed char __width, unsigned char __prec, char *__s);
+double xround(double v, int p);
 
-void dotest(double v, int w, int p)
+#define TEST_XROUND(vi, rn, vc) \
+  { double vo = xround((vi), (rn)) ; if(vo==(vc)) { printf("%15.5lf == %15.5lf SUCCEEDED\n", vo, vc); } \
+  else { printf("%15.5lf != %15.5lf FAILED, vo, vc\n"); } }
+
+void test_xround()
+{
+  double v2;
+
+  TEST_XROUND(12.34567,  0, 12.0);
+//  TEST_XROUND(12.34567, -1, 12.3);
+//  TEST_XROUND(12.34567, -2, 12.35);
+//  TEST_XROUND(12.34567, -3, 12.346);
+//  TEST_XROUND(12.34567, -4, 12.3457);
+//  TEST_XROUND(123456.7,  0, 123457);
+//  TEST_XROUND(123456.7,  1, 123460);
+//  TEST_XROUND(123456.7,  2, 123500);
+//  TEST_XROUND(123456.7,  3, 124000);d
+//  TEST_XROUND(123456.7,  4, 120000);
+
+
+
+  v2 = xround(v, -p);
+  printf("v=%.10f, v2=%.10f\n", v, v2);
+
+  return;
+}
+
+
+void dotest_dtostrf(double v, int w, int p)
 {
   char fmt[32], str1[1024], str2[1024];
 
@@ -16,9 +45,19 @@ void dotest(double v, int w, int p)
   printf(">>%25s<< %s >>%-25s<<\n", str1, strcmp(str1, str2)?"!=":"==", str2);
 }
 
+void test_dtostrf()
+{
+  dotest_dtostrf(123.456, 7, 0);
+  dotest_dtostrf(123.456, 7, 1);
+  dotest_dtostrf(123.456, 7, 2);
+  dotest_dtostrf(234.567, 7, 1);
+  dotest_dtostrf(234.567, 0, 2);
+  return;
+}
+
 int main(int argc, char **argv)
 {
-  dotest(123.456, 7, 1);
+  test_xround();
   return 0;
 }
 
